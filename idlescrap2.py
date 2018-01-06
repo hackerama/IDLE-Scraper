1a3722202921a456dc8c6b187fd67931bfd8c32e
root <root@localhost.localdomain>
2018-01-06 10:30:32 -0500

    scraper

--- idlescrap2.py
+++ idlescrap2.py
@@ -73,17 +73,119 @@ def parseCivil():
     return servFeat
 
 
+def parseConf():
+    
+    global servFeat
+
+    nome = bs.find_all('td', {'class' : 'colunaValor'})
+    stripWork(nome, 0, 0)
+
+    cpf = bs.find_all('td', {'class' : 'colunaValor'})
+    stripWork(cpf, 1, 0)
+
+    serv = bs.find_all('td', {'class' : 'colunaValor'})
+    stripWork(serv, 2, 0)
+    
+    mat = bs.find_all('strong')
+    stripWork(mat, 26, 0)
+
+    cargo = bs.find_all('strong')
+    stripWork(cargo, 27, 0)
+    
+    #orgao origem - lotacao
+
+    uorg = bs.find_all('strong')
+    stripWork(uorg, 33, 0)
+
+    orgao = bs.find_all('strong')
+    stripWork(orgao, 34, 0)
+
+    orgaoS = bs.find_all('strong')
+    stripWork(orgaoS, 35, 0)
+
+    #local de exercicio
+    
+    uf = bs.find_all('strong')
+    stripWork(uf, 37, 0)
+
+    uorgx = bs.find_all('strong')
+    stripWork(uorgx, 38, 0)
+
+    orgaox = bs.find_all('strong')
+    stripWork(orgaox, 39, 0)
+
+
+    orgaoSx = bs.find_all('strong')
+    stripWork(orgaoSx, 40, 0)
+    
+    # outras informacoes
+
+    reg = bs.find_all('strong')
+    stripWork(reg, 41, 0)
+    
+    status = bs.find_all('strong')
+    stripWork(status, 42, 0)
+
+
+    jorn = bs.find_all('strong')
+    stripWork(jorn, 44, 0)
+
+    return servFeat
+
+def parseMil():
+
+    global servFeat
+
+    nome = bs.find_all('td', {'class' : 'colunaValor'})
+    stripWork(nome, 0, 0)
+
+    cpf = bs.find_all('td', {'class' : 'colunaValor'})
+    stripWork(cpf, 1, 0)
+
+    serv = bs.find_all('td', {'class' : 'colunaValor'})
+    stripWork(serv, 2, 0)
+
+    mat = bs.find_all('strong')
+    stripWork(mat, 0, 0)
+
+    posto = bs.find_all('strong')
+    stripWork(posto, 1, 0)
+
+    orgao = bs.find_all('strong')
+    stripWork(orgao, 2, 0)
+
+    orgaoS = bs.find_all('strong')
+    stripWork(orgaoS, 3, 0)
+
+    reg = bs.find_all('strong')
+    stripWork(reg, 4, 0)
+
+    status = bs.find_all('strong')
+    stripWork(status, 5, 0)
+
+    jorn = bs.find_all('strong')
+    stripWork(jorn, 7, 0)
+
+    return servFeat
+
+
 c = csv.writer(open("servcivil.csv", "ab"))
+cm = csv.writer(open("servMilitar.csv", "ab"))
 c.writerow([
     "Nome","CPF","Servidor","Matricula","Cargo","O. Origem - UORG",
     "O. Origem - Orgao", "O. Origem - Orgao Superior", 
     "Local de Exercicio - UF", "Local de Exercicio - UORG", 
     "Local de Exercicio - Orgao","Local de Exercicio - Orgao Superior",
     "Regime Juridico", "Situacao Vinculo", "Jornada de Trabalho"
-    ])
+	])
 
+cm.writerow([
+    "Nome","CPF","Servidor","Matricula","Posto - Graduacao","Orgao",
+	"Orgao Superior", "Regime Juridico", "Situacao Vinculo", 
+	"Jornada de Trabalho"
+	])
 
-for idServ in range(1000002, 1000020):
+for idServ in range(1406910, 1406960):
     servFeat = []
     idServ = str(idServ)
     print idServ
@@ -94,15 +196,22 @@ for idServ in range(1000002, 1000020):
         bs = BeautifulSoup(html, 'lxml')
         print idServ, resp.code
         serv = bs.find_all('td', {'class': 'colunaValor'})
+        cargo = bs.find_all('strong')
         work = serv[2].contents[0]
         if work.strip() == 'Civil':
-            parseCivil()
+            if cargo[1].contents == []:
+                parseConf()
+            else:
+                parseCivil()
             try:
                 c.writerow(servFeat)
             except UnicodeEncodeError:
                 c.writerow("ERROR")  #gambiarra
                 continue
             print work.strip()
-    
+        else: 
+            parseMil()
+            cm.writerow(servFeat)
+
     else:
         pass
