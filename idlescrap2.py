d57299509279903eb6a011c861f37ccd35a81f6a
root <root@localhost.localdomain>
2018-01-05 19:10:48 -0500

    scraper

--- idlescrap2.py
+++ idlescrap2.py
@@ -3,10 +3,20 @@ import csv
 import urllib
 from bs4 import BeautifulSoup
 
+def worko(fetch, x, y):
+
+    if fetch[x].contents == []:
+       return servFeat.append('NULL')
+
+    else:
+        work = fetch[x].contents[y]
+        servFeat.append(work.strip())
+
+
 def parseCivil():
     
-    servFeat = []
-    
+    global servFeat
+
     nome = bs.find_all('td', {'class' : 'colunaValor'})
     work =  nome[0].contents[0]
     servFeat.append(work.strip())
@@ -41,16 +51,55 @@ def parseCivil():
     work = orgaoS[9].contents[0]
     servFeat.append(work.strip())
 
-    c.writerow(servFeat)
+    #local de exercicio
+    
+    uf = bs.find_all('strong')
+    worko(uf, 11, 0)
 
+
+    uorg = bs.find_all('strong')
+    if uorg[12].contents == []:
+        servFeat.append('null')
+    else:    
+        work = uorg[12].contents[0]
+        servFeat.append(work.strip())
+
+    orgao = bs.find_all('strong')
+    work = orgao[13].contents[0]
+    servFeat.append(work.strip())
+
+    orgaoS = bs.find_all('strong')
+    work = orgaoS[14].contents[0]
+    servFeat.append(work.strip())
+    
+    # outras informacoes
+
+    reg = bs.find_all('strong')
+    work = reg[15].contents[0]
+    servFeat.append(work.strip())
+    
+    status = bs.find_all('strong')
+    work = status[16].contents[0]
+    servFeat.append(work.strip())
+
+    jorn = bs.find_all('strong')
+    work = jorn[18].contents[0]
+    servFeat.append(work.strip())
+
+    return servFeat
 
 
 c = csv.writer(open("servcivil.csv", "ab"))
-c.writerow(["Nome","CPF","Servidor","Matricula","Cargo","O. Origem - UORG",
-    "O. Origem - Orgao", "O. Origem - Orgao Superior"])
+c.writerow(["Nome","CPF","Servidor","Matricula","Cargo",
+    "O. Origem - UORG","O. Origem - Orgao", "O. Origem - Orgao Superior", 
+    "Local de Exercicio - UF", "Local de Exercicio - UORG", 
+    "Local de Exercicio - Orgao","Local de Exercicio - Orgao Superior",
+    "Regime Juridico", "Situacao Vinculo", "Jornada de Trabalho"
+    ])
 
 
 for idServ in range(1000002, 1000010):
+    servFeat = []
     idServ = str(idServ)
     print idServ
     resp = urllib.urlopen('http://www.portaltransparencia.gov.br/servidores/Servidor-DetalhaServidor.asp?IdServidor=' + idServ)
@@ -63,6 +112,7 @@ for idServ in range(1000002, 1000010):
         work = serv[2].contents[0]
         if work.strip() == 'Civil':
             parseCivil()
+            c.writerow(servFeat)
             print work.strip()
     
     else:
